39e3617728d53acbe24c12e041482cded1ed7051
tony <tony.spicer.covin@gmail.com>
2019-10-09 20:28:34 -0500

    working on results fragment

---
+++ app/src/main/java/com/tony/wouldyou/ResultFragment.java
@@ -0,0 +1,31 @@
+package com.tony.wouldyou;
+
+
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class resultFragment extends Fragment {
+
+
+    public resultFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_result, container, false);
+    }
+
+}
